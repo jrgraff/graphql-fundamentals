465e6e9ca98f62c660ffb08ada99f2b357145d91
Jackson Graff <jacksongraff@hotmail.com>
2021-09-10 11:05:29 -0300

    mutation

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -5,12 +5,33 @@ import { User } from "../utils/types";
 
 export type UserArgsProps = {
   id: string;
+  name: string
+  email: string
+  age: number
+}
+
+export type PaymentMethodArgsProps = {
+  id: string
+  userId: string
+  last4: string
+  expMonth: number
+  expYear: number
 }
 
 export const typeDefs = gql`
   type Query {
     getUser: User
     getUserById(id: ID!): User
+  }
+  type Mutation {
+    createUser(id: ID!, name: String, email: String, age: Int): User
+    createPaymentMethod(
+      id: ID!, 
+      userId: ID!,
+      last4: String, 
+      expMonth: Int, 
+      expYear: Int
+    ): PaymentMethod
   }
   type User {
     id: ID!
@@ -21,6 +42,7 @@ export const typeDefs = gql`
   }
   type PaymentMethod {
     id: ID!
+    userId: ID!
     last4: String
     expMonth: Int
     expYear: Int
@@ -38,11 +60,22 @@ export const resolvers = {
     getUserById: (parent: any, args: UserArgsProps) => {
       const { id } = args;
       return users.find((user) => user.id === id)
-    }
+    },
+  },
+  Mutation: {
+    createUser: (parents: any, args: UserArgsProps) => {
+      const newUser: User = args
+      users.push(newUser);
+      return newUser
+    },
+    createPaymentMethod: (parent: any, args: PaymentMethodArgsProps) => {
+      paymentMethods.push(args)
+      return args
+    },
   },
   User: {
     paymentMethods: (parent: User) => {
       return paymentMethods.filter((payment) => payment.userId === parent.id)
-    }
-  }
+    },
+  },
 }
