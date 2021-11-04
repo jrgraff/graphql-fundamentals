bd4058d1813394c60ecda3656495ccd0e83461d8
Jackson Graff <jacksongraff@hotmail.com>
2021-11-04 18:32:23 -0300

    changing from rest to graphql

--- integrando_com_aplicacao/agenda-petshop-front/src/api/clientes.js
+++ integrando_com_aplicacao/agenda-petshop-front/src/api/clientes.js
@@ -1,29 +1,43 @@
-import { api } from './config'
+import { opcoesFetch } from './config'
 
-const listarClientes = () => 
-  api
-    .get('/clientes')
-    .then(resposta => resposta.data)
+const listarClientes = () =>
+  fetch('http://localhost:4000', opcoesFetch('{clientes {id nome cpf}}'))
+    .then(resp => resp.json())
+    .then(dados => dados.data.clientes)
 
-const buscarClientePorId = id => 
-  api
-    .get(`/clientes/cliente/${id}`)
-    .then(resposta => resposta.data[0])
+const buscarClientePorId = id =>
+  fetch('http://localhost:4000', opcoesFetch(`{cliente(id: ${id}) {nome cpf}}`))
+    .then(resp => resp.json())
+    .then(dados => dados.data.cliente)
 
-const adicionarCliente = cliente => 
-  api
-    .post('/clientes/cliente', cliente)
-    .then(resposta => resposta.data)
+const adicionarCliente = cliente =>
+  fetch('http://localhost:4000', opcoesFetch(`
+  mutation {
+    adicionarCliente(nome: "${cliente.nome}")
+  }
+`))
+    .then(resp => resp.json())
+    .then(dados => dados.data.clientes)
 
 const alterarCliente = (id, cliente) =>
-  api
-    .put(`/clientes/cliente/${id}`, cliente)
-    .then(resposta => resposta.data)
+  fetch('http://localhost:4000', opcoesFetch(`
+  mutation {
+    atualizarCliente(id: "${id}", nome: "${cliente.nome}", cpf: "${cliente.cpf}") {
+      nome
+    }
+  }
+`))
+    .then(resp => resp.json())
+    .then(dados => dados.data)
 
-const removerCliente = id => 
-  api
-    .delete(`/clientes/cliente/${id}`)
-    .then(resposta => resposta.data)
+const removerCliente = id =>
+  fetch('http://localhost:4000', opcoesFetch(`
+    mutation {
+      deletarCliente(id: ${id})
+    }
+  `))
+    .then(resp => resp.json())
+    .then(dados => dados.data.clientes)
 
 export default {
   listarClientes,
--- integrando_com_aplicacao/agenda-petshop-front/src/api/config.js
+++ integrando_com_aplicacao/agenda-petshop-front/src/api/config.js
@@ -1,9 +1,19 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'http://localhost:3333',
   timeout: 10000,
   headers: {
     'content-type': 'application/json',
-  }
+  },
+})
+
+export const opcoesFetch = (query) => ({
+  method: 'post',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query
+    })
 })
