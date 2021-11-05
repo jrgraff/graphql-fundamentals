25bea586295d2faf81a23aebf4d6b651f03a2226
Jackson Graff <jacksongraff@hotmail.com>
2021-11-05 14:13:05 -0300

    mutations no apollo

--- integrando_com_aplicacao/agenda-petshop-front/src/api/clientes.js
+++ integrando_com_aplicacao/agenda-petshop-front/src/api/clientes.js
@@ -1,43 +1,45 @@
-import { opcoesFetch } from './config'
+import { api, opcoesFetch } from './config'
 
 const listarClientes = () =>
-  fetch('http://localhost:4000', opcoesFetch('{clientes {id nome cpf}}'))
-    .then(resp => resp.json())
+  fetch('http://localhost:4000/', opcoesFetch('{clientes { id nome cpf }}'))
+    .then(resposta => resposta.json())
     .then(dados => dados.data.clientes)
 
 const buscarClientePorId = id =>
-  fetch('http://localhost:4000', opcoesFetch(`{cliente(id: ${id}) {nome cpf}}`))
-    .then(resp => resp.json())
+  fetch('http://localhost:4000', opcoesFetch(`{
+    cliente(id: ${id}) {
+      nome
+      cpf
+    }
+  }`))
+    .then(resposta => resposta.json())
     .then(dados => dados.data.cliente)
 
 const adicionarCliente = cliente =>
   fetch('http://localhost:4000', opcoesFetch(`
-  mutation {
-    adicionarCliente(nome: "${cliente.nome}")
-  }
-`))
-    .then(resp => resp.json())
-    .then(dados => dados.data.clientes)
+    mutation {
+      adicionarCliente(nome: "${cliente.nome}", cpf: "${cliente.cpf}") {
+        id
+        nome
+      }
+    }
+  `))
+    .then(resposta => resposta.json())
+    .then(dados => dados.data.cliente)
 
 const alterarCliente = (id, cliente) =>
   fetch('http://localhost:4000', opcoesFetch(`
-  mutation {
-    atualizarCliente(id: "${id}", nome: "${cliente.nome}", cpf: "${cliente.cpf}") {
-      nome
+    mutation {
+      atualizarCliente(id: ${id}, nome: "${cliente.nome}", cpf: "${cliente.cpf}") {
+        nome
+      }
     }
-  }
-`))
-    .then(resp => resp.json())
+  `))
+    .then(resposta => resposta.json())
     .then(dados => dados.data)
 
 const removerCliente = id =>
-  fetch('http://localhost:4000', opcoesFetch(`
-    mutation {
-      deletarCliente(id: ${id})
-    }
-  `))
-    .then(resp => resp.json())
-    .then(dados => dados.data.clientes)
+  api.delete(`/clientes/cliente/${id}`).then(resposta => resposta.data)
 
 export default {
   listarClientes,
